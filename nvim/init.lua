6ca0499784a51198c3853aeb4ccea6fbf9a70d2b
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2024-08-24 17:56:02 +0100

    Added noice for better GUI looks in neovim.

--- nvim/init.lua
+++ nvim/init.lua
@@ -313,6 +313,7 @@ require("lazy").setup({
     --0=============================================================================================0
     {
     'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         vim.o.showmode = false
         require('lualine').setup({
@@ -672,6 +673,36 @@ require("lazy").setup({
       lazy = false,
       priority = 1000,
       opts = {}
+    },
+    {"MunifTanjim/nui.nvim"},
+    {"rcarriga/nvim-notify"},
+    {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify"
+      },
+      config = function()
+        require("noice").setup({
+          lsp = {
+            -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+            override = {
+              ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+              ["vim.lsp.util.stylize_markdown"] = true,
+              ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+            },
+          },
+          -- you can enable a preset for easier configuration
+          presets = {
+            bottom_search = true, -- use a classic bottom cmdline for search
+            command_palette = true, -- position the cmdline and popupmenu together
+            long_message_to_split = true, -- long messages will be sent to a split
+            inc_rename = false, -- enables an input dialog for inc-rename.nvim
+            lsp_doc_border = false, -- add a border to hover docs and signature help
+          },
+        })
+      end
     }
 },
 {
