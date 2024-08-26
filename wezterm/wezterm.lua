605265eaf5ea1a157d725b51c9602608810fc0f3
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2024-08-26 16:24:28 +0100

    Removed all padding in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,5 +8,11 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 10
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 return config
