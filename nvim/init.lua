1c55e09da96ac88a25f29ee5abf19a45f0003e3d
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-04-10 16:09:38 +0100

    Fixed theme imports being ignored

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,5 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require "options"
 require "keymaps"
 
-require("lazy").setup("plugins")
-require("lazy").setup("themes")
+require("lazy").setup({
+  { import = "plugins" },
+  { import = "themes" },
+})
