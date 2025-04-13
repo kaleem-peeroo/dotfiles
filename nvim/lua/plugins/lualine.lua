3227dafc09c3960c77ab79552649ead3996cdd85
Kaleem Peeroo <kaleempeeroo@Mac.home>
2025-04-13 16:48:19 +0100

    Moved lua line to top

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,16 +2,17 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        vim.o.showmode = false
         require('lualine').setup({
             options = {
                 icons_enabled = true,
-                -- theme = 'dracula',
+                theme = 'auto',
             },
             -- this part shows full path, helps navigate in Oil.
-            sections = {
-                lualine_c = { { 'filename', path = 2 } }
-            }
+            tabline = {
+                lualine_a = { 'branch' },
+                lualine_b = { { 'filename', path = 1 } },
+            },
+            sections = {}
         })
     end
 }
