d34f188a028ccecc0a9a76142ca0d735ea3033a2
Kaleem Peeroo <kaleempeeroo@Mac.home>
2025-04-13 18:57:42 +0100

    Added python to treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,11 +5,11 @@ return {
         -- ENABLES THIS IF USING WINDOWS:
         -- require('nvim-treesitter.install').compilers = { 'zig' } 
         require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'latex' },
+            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'latex', 'python'},
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
             -- Automatically install missing parsers when entering buffer
-            auto_install = false,
+            auto_install = true,
             highlight = {
                 enable = true,
             },
