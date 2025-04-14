d926fe25450b0488237f3b87e360748352c0c083
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-04-14 11:59:19 +0100

    Remoed python from tree-sitter because it was fading out variabels

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         -- ENABLES THIS IF USING WINDOWS:
         -- require('nvim-treesitter.install').compilers = { 'zig' } 
         require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'latex', 'python'},
+            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'latex'},
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
             -- Automatically install missing parsers when entering buffer
