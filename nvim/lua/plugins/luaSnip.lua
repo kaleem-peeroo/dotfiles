23406cef02b736bddf2373601bc05e88cb662fa2
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-05-12 19:35:39 +0100

    Added luasnip

--- nvim/lua/plugins/luaSnip.lua
+++ nvim/lua/plugins/luaSnip.lua
@@ -8,5 +8,14 @@ return {
         require("luasnip.loaders.from_vscode").lazy_load({
             paths = './my_snippets'
         })
+        require("luasnip.loaders.from_snipmate").lazy_load()
+        require("luasnip.loaders.from_lua").load({
+            paths = "~/.config/nvim/LuaSnip/"
+        })
+
+        require("luasnip").config.set_config({
+            enable_autosnippets = true,
+            store_selection_keys = "<Tab>",
+        })
     end
 }
