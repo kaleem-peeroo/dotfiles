61b4eb467b44fce87ac788f3386b175654f5b6b0
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-06-09 17:46:18 +0100

    Updated mason to work

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,15 +1,4 @@
 return {
     "mason-org/mason.nvim",
-    "mason-org/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    opts = {
-        ensure_installed = {
-            "black",
-            "debugpy",
-            "mypy"
-        },
-    },
-    config = function(opts)
-        require("mason").setup()
-    end
+    opts = {},
 }
