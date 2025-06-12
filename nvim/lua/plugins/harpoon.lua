7efced29fac7a781b47df8d333c34088f456a1ba
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-06-12 17:18:17 +0100

    Changed hot keys for harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -46,9 +46,9 @@ return {
         )
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
 
         vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end)
         vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end)
