17b28a55f60a220e93cf5c57fe0a655689c6d0f9
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-06-14 17:09:55 +0100

    Updated keybinds and pytest args for neotest

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -13,39 +13,39 @@ return {
                 require("neotest-python")({
                     runner = "pytest",
                     python = "/Users/kaleem/venv/bin/python",
-                    args = { "--disable-warnings" },
+                    args = { "--disable-warnings", "-s" },
                 }),
             },
             status = {
                 virtual_text = true,
             },
+            output = {
+                open_on_run_with_errors = true,
+                strategy = "split",
+                location = "bottom"
+            }
         }
     end,
     keys = {
         {
-            "<leader>t",
+            "t",
             function() require("neotest").run.run() end,
             desc = "Run nearest test"
         },
         {
-            "<leader>T",
+            "T",
             function() require("neotest").run.run(vim.fn.expand("%")) end,
             desc = "Run all tests in file"
         },
         {
-            "<leader>tv",
+            "tv",
             function() require("neotest").summary.toggle() end,
             desc = "Toggle test summary"
         },
         {
-            "<leader>to",
+            "to",
             function() require("neotest").output.open() end,
             desc = "Open test output"
         },
-        {
-            "<leader>td",
-            function() require("neotest").run.run({ strategy = "dap" }) end,
-            desc = "Debug nearest test"
-        }
     }
 }
