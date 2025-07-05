59476d28d4d66fdc08ac33157b0f16d75d24f817
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-07-05 15:59:58 +0100

    Renamed nvim-treesitter file

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -3,11 +3,11 @@ return {
     build = ":TSUpdate",
     config = function()
         -- ENABLES THIS IF USING WINDOWS:
-        -- require('nvim-treesitter.install').compilers = { 'zig' } 
+        -- require('nvim-treesitter.install').compilers = { 'zig' }
         require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'latex', 'python'},
+            ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'latex', 'python' },
             -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+            sync_install = true,
             -- Automatically install missing parsers when entering buffer
             auto_install = true,
             highlight = {
