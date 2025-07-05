3bf2b30287f42bc12d3c4f6435a16328c3f51135
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-07-05 15:59:43 +0100

    Renamed vimtex file

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -3,5 +3,8 @@ return {
     lazy = false,
     init = function()
         vim.g.vimtex_view_method = 'zathura'
-    end
+    end,
+    keys = {
+        "<leader>lv", "<cmd>VimtexView<cr>", desc = "Vimtex View"
+    }
 }
