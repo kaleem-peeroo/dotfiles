5bd13f0cbcecc8d838d338576586203a6ef17888
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-07-09 11:41:12 +0100

    Added keybindings in obsidian nvim for toggling checkboxes - C-x

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -7,7 +7,6 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope.nvim",
         "hrsh7th/nvim-cmp",
-        "nvim-treesitter/nvim-treesitter",
     },
     opts = {
         workspaces = {
@@ -19,7 +18,18 @@ return {
         new_notes_location = "current_dir",
         use_advanced_uri = true,
         ui = {
+            enable = false,
             checkboxes = {}
         }
+    },
+    keys = {
+        vim.keymap.set("n", "<C-x>", function()
+            local line = vim.api.nvim_get_current_line()
+            if line:match("%[ %]") then
+                vim.cmd("s/\\[ \\]/[x]/")
+            elseif line:match("%[x%]") then
+                vim.cmd("s/\\[x\\]/[ ]/")
+            end
+        end, { desc = "Toggle Markdown checkbox [ ] <-> [x]" })
     }
 }
