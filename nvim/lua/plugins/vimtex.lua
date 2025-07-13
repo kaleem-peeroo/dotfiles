d4b38a084e77c306e9155b9c4859311cba4455d5
Kaleem Peeroo <kaleempeeroo@Mac.home>
2025-07-13 12:32:18 +0100

    Changed binding for vimtexview

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -5,6 +5,6 @@ return {
         vim.g.vimtex_view_method = 'zathura'
     end,
     keys = {
-        "<leader>lv", "<cmd>VimtexView<cr>", desc = "Vimtex View"
+        "<leader>v", "<cmd>VimtexView<cr>", desc = "Vimtex View"
     }
 }
