7d02fe0fd084decd3d32f5c8603e53877f98f06c
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2025-07-14 12:19:17 +0100

    Updated smear cusor config

--- nvim/lua/plugins/smear-cursor.lua
+++ nvim/lua/plugins/smear-cursor.lua
@@ -1,4 +1,9 @@
 return {
     "sphamba/smear-cursor.nvim",
-    opts = {}
+    opts = {
+        cursor_color = "#aed987",
+        legacy_computing_symbols_support = true,
+        hide_target_hack = true,
+        never_draw_over_target = true
+    }
 }
