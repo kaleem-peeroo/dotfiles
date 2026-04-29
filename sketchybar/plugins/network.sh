bb9b0c5a41e73cf010cb5263d5b30afe89fbde86
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2026-04-29 19:27:48 +0100

    sketchybar: updated network to show from Kbps to Mbps

--- sketchybar/plugins/network.sh
+++ sketchybar/plugins/network.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# Uses awk printf %d to convert floats to integers
-UP_DOWN=$(ifstat -i en0 1 1 | tail -1 | awk '{printf "↓%dKB/s ↑%dKB/s", $1, $2}')
+# Uses awk printf to divide by 1000 and format to 1 decimal place
+UP_DOWN=$(ifstat -i en0 1 1 | tail -1 | awk '{printf "↓%.1fMbps ↑%.1fMbps", $1/1000, $2/1000}')
 
 sketchybar --set $NAME label="$UP_DOWN"
