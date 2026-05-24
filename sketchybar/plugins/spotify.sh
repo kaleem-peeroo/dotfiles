eb0efaaf4165375a940a4630fc381d6d8270c75a
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2026-05-24 23:32:46 +0100

    sketchybar: added spotify indicator

---
+++ sketchybar/plugins/spotify.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SPOTIFY_DATA=$(osascript -e '
+tell application "System Events"
+    set isRunning to (count of (every process whose name is "Spotify")) > 0
+end tell
+if isRunning then
+    tell application "Spotify"
+        if player state is playing then
+            return "playing"
+        else
+            return "paused"
+        end if
+    end tell
+else
+    return "stopped"
+end if
+')
+
+if [ "$SPOTIFY_DATA" = "playing" ]; then
+  sketchybar --set "$NAME" icon=":spotify:" label="􀊄" drawing=on
+elif [ "$SPOTIFY_DATA" = "paused" ]; then
+  sketchybar --set "$NAME" icon=":spotify:" label="􀊆" drawing=on
+else
+  sketchybar --set "$NAME" drawing=off
+fi
