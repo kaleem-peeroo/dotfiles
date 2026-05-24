d0c29f7fb30e999763ca25bb7e1e946b7f11bf03
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2026-05-24 22:33:15 +0100

    sketchybar: discord icon turns purple when unread

--- sketchybar/plugins/discord.sh
+++ sketchybar/plugins/discord.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+source "$CONFIG_DIR/colors.sh"
 
 UNREAD=$(lsappinfo info -only StatusLabel com.hnc.Discord | grep -o '"label"="[^"]*"' | cut -d'"' -f4)
 
-if [ -z "$UNREAD" ] || [ "$UNREAD" = "" ]; then
-  sketchybar --set $NAME label="0"
+if [ -z "$UNREAD" ] || [ "$UNREAD" = "" ] || [ "$UNREAD" = "0" ]; then
+  sketchybar --set $NAME label="0" \
+                     icon.color=$WHITE
 else
-  sketchybar --set $NAME label="$UNREAD"
+  sketchybar --set $NAME label="$UNREAD" \
+                     icon.color=$ACCENT_COLOR
 fi
