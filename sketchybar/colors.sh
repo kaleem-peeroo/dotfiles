5b18d7ba063432b0fa1d4b644c80ff468b8e27e2
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2026-07-25 20:09:11 +0100

    feat(sketchybar): set color scheme from purple to blue

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -13,9 +13,9 @@ export WHITE=0xffffffff
 # export ACCENT_COLOR=0xffffffff
 
 -- Purple Scheme --
-export BAR_COLOR=0xff140c42
-export ITEM_BG_COLOR=0xff2b1c84
-export ACCENT_COLOR=0xffeb46f9
+# export BAR_COLOR=0xff140c42
+# export ITEM_BG_COLOR=0xff2b1c84
+# export ACCENT_COLOR=0xffeb46f9
 
 # -- Red Scheme ---
 # export BAR_COLOR=0xff23090e
@@ -23,9 +23,9 @@ export ACCENT_COLOR=0xffeb46f9
 # export ACCENT_COLOR=0xffff2453
 
 # -- Blue Scheme ---
-# export BAR_COLOR=0xff021254
-# export ITEM_BG_COLOR=0xff093aa8
-# export ACCENT_COLOR=0xff15bdf9
+export BAR_COLOR=0xff021254
+export ITEM_BG_COLOR=0xff093aa8
+export ACCENT_COLOR=0xff15bdf9
 
 # -- Green Scheme --
 # export BAR_COLOR=0xff003315
