780b2f0f8467cc4b4977e0f92004dfbc6db02951
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2026-07-26 14:04:18 +0100

    feat(nvim-lspconfig): added lsp config for ts and angular

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,112 +1,127 @@
 return {
-    'neovim/nvim-lspconfig',
-    config = function()
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+  'neovim/nvim-lspconfig',
+  config = function()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-        local custom_attach = function(client, bufnr)
-        end
-
-        -- Pyright
-        vim.lsp.config('pyright', {
-            on_attach = custom_attach,
-            capabilities = capabilities,
-            settings = {
-                python = {
-                    pythonPath = "/Users/kaleempeeroo/venv/bin/python",
-                    analysis = {
-                        autoSearchPaths = true,
-                        useLibraryCodeForTypes = false,
-                        diagnosticMode = "openFilesOnly",
-                    },
-                },
-            },
-        })
-        vim.lsp.enable('pyright')
-
-        -- GO
-        vim.lsp.config('gopls', {
-            on_attach = custom_attach,
-            capabilities = capabilities,
-            settings = {
-                gopls = {
-                    analyses = {
-                        unusedparams = true,
-                    },
-                    staticcheck = true,
-                    gofumpt = true,
-                },
-            }
-        })
-        vim.lsp.enable('gopls')
-
-        -- Ruff
-        vim.lsp.config('ruff', {
-            on_attach = custom_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('ruff')
-
-        -- Rust Analyzer
-        vim.lsp.config('rust_analyzer', {
-            on_attach = custom_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('rust_analyzer')
-
-        -- Texlab
-        vim.lsp.config('texlab', {
-            on_attach = custom_attach,
-            capabilities = capabilities,
-        })
-        vim.lsp.enable('texlab')
-
-        -- Lua LS
-        vim.lsp.config('lua_ls', {
-            on_attach = custom_attach,
-            capabilities = capabilities,
-            settings = {
-                Lua = {
-                    runtime = {
-                        version = 'LuaJIT',
-                    },
-                    diagnostics = {
-                        enable = false,
-                    },
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME,
-                        },
-                    },
-                    telemetry = { enable = false },
-                },
-            },
-        })
-        vim.lsp.enable('lua_ls')
-
-        -- Clangd (C/C++)
-        vim.lsp.config('clangd', {
-            on_attach = custom_attach,
-            capabilities = capabilities,
-            cmd = {
-                "clangd",
-                "--background-index",
-                "--clang-tidy",
-                "--log=verbose",
-            },
-            init_options = {
-                fallbackFlags = { "-std=c++17" },
-            },
-        })
-        vim.lsp.enable('clangd')
-
-        -- New server template:
-        -- vim.lsp.config('SERVER', {
-        --     on_attach = custom_attach,
-        --     capabilities = capabilities,
-        --     -- COPY PASTE SERVER SETTINGS HERE
-        -- })
-        -- vim.lsp.enable('SERVER')
+    local custom_attach = function(client, bufnr)
     end
+
+    -- Pyright
+    vim.lsp.config('pyright', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+      settings = {
+        python = {
+          pythonPath = "/Users/kaleempeeroo/venv/bin/python",
+          analysis = {
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = false,
+            diagnosticMode = "openFilesOnly",
+          },
+        },
+      },
+    })
+    vim.lsp.enable('pyright')
+
+    -- GO
+    vim.lsp.config('gopls', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+      settings = {
+        gopls = {
+          analyses = {
+            unusedparams = true,
+          },
+          staticcheck = true,
+          gofumpt = true,
+        },
+      }
+    })
+    vim.lsp.enable('gopls')
+
+    -- Ruff
+    vim.lsp.config('ruff', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+    })
+    vim.lsp.enable('ruff')
+
+    -- Rust Analyzer
+    vim.lsp.config('rust_analyzer', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+    })
+    vim.lsp.enable('rust_analyzer')
+
+    -- Texlab
+    vim.lsp.config('texlab', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+    })
+    vim.lsp.enable('texlab')
+
+    -- Lua LS
+    vim.lsp.config('lua_ls', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          runtime = {
+            version = 'LuaJIT',
+          },
+          diagnostics = {
+            enable = false,
+          },
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+            },
+          },
+          telemetry = { enable = false },
+        },
+      },
+    })
+    vim.lsp.enable('lua_ls')
+
+    -- Clangd (C/C++)
+    vim.lsp.config('clangd', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--log=verbose",
+        "--fallback-style={BasedOnStyle: LLVM, IndentWidth: 4}",
+      },
+      init_options = {
+        fallbackFlags = { "-std=c++17" },
+      },
+    })
+    vim.lsp.enable('clangd')
+
+    -- TypeScript / JavaScript (ts_ls)
+    vim.lsp.config('ts_ls', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+    })
+    vim.lsp.enable('ts_ls')
+
+    -- Angular Language Server (angularls)
+    vim.lsp.config('angularls', {
+      on_attach = custom_attach,
+      capabilities = capabilities,
+    })
+    vim.lsp.enable('angularls')
+
+    -- New server template:
+    -- vim.lsp.config('SERVER', {
+    --     on_attach = custom_attach,
+    --     capabilities = capabilities,
+    --     -- COPY PASTE SERVER SETTINGS HERE
+    -- })
+    -- vim.lsp.enable('SERVER')
+  end
 }
