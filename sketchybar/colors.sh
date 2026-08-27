8e4a0d9763d9738ec491cea878b2050d950f3fee
Kaleem Peeroo <KaleemPeeroo@gmail.com>
2026-08-27 10:53:49 +0100

    fix(sketchybar): comment out purple scheme header in colors.sh

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -12,7 +12,7 @@ export WHITE=0xffffffff
 # export ITEM_BG_COLOR=0xff353c3f
 # export ACCENT_COLOR=0xffffffff
 
--- Purple Scheme --
+# -- Purple Scheme --
 # export BAR_COLOR=0xff140c42
 # export ITEM_BG_COLOR=0xff2b1c84
 # export ACCENT_COLOR=0xffeb46f9
